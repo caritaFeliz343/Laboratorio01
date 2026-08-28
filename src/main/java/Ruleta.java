cc137b2830e3832245e9b1e07dec247248a8b2ec
Christian Espinoza <c.espinoza33@ufromail.cl>
2026-08-28 09:32:07 -0400

    Agregar metodos main y menu, ademas de estructura base de la ruleta

--- src/main/java/Ruleta.java
+++ src/main/java/Ruleta.java
@@ -1,2 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Ruleta {
+    public static final int MAX_HISTORIAL = 100;
+    public static int[] historialNumeros = new int[MAX_HISTORIAL];
+    public static int[] historialApuestas = new int[MAX_HISTORIAL];
+    public static boolean[] historialAciertos = new boolean[MAX_HISTORIAL];
+    public static int historialSize = 0;
+    public static Random rng = new Random();
+    public static int[] numerosRojos = {
+            1, 3, 5, 7, 9, 12, 14, 16, 18,
+            19, 21, 23, 25, 27, 30, 32, 34, 36
+    };
+
+    /**
+     * Método principal: inicia el programa llamando al menú.
+     */
+    public static void main(String[] args) {
+        menu();
+    }
+
+    /**
+     * Controla el flujo principal del programa mostrando
+     * un menú en consola.
+     */
+    public static void menu() {
+// TODO: Repetir el menú hasta que el usuario elija salir.
+    }
 }
