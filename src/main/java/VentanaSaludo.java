eac3a68b09d20490f8df97ae569e7a724b3b1204
Christian Espinoza <c.espinoza33@ufromail.cl>
2026-09-07 18:22:04 -0300

    Agregar metodo irARegistro e implementar boton para registrarse

--- src/main/java/VentanaSaludo.java
+++ src/main/java/VentanaSaludo.java
@@ -7,6 +7,7 @@ public class VentanaSaludo
     private final JLabel txtBienvenida = new JLabel("Bienvenido al casino Black Cat");
     private final JLabel txtOpciones = new JLabel("¿Que deseas hacer?");
     private final JButton btnLogin = new JButton("Iniciar sesion");
+    private final JButton btnRegistro = new JButton("Registrarse");
     public VentanaSaludo()
     {
         frame.setSize(640,480);
@@ -18,6 +19,8 @@ public class VentanaSaludo
         frame.add(txtOpciones);
         frame.add(btnLogin);
         btnLogin.addActionListener(e -> irALogin());
+        frame.add(btnRegistro);
+        btnRegistro.addActionListener(e -> irARegistro());
     }
     public void mostrarVentanaSaludo()
     {
@@ -27,7 +30,13 @@ public class VentanaSaludo
     private void irALogin()
     {
         frame.dispose();
-        VentanaLogin login = new VentanaLogin();
-        login.mostrarVentana();
+        VentanaLogin ventanaLogin = new VentanaLogin();
+        ventanaLogin.mostrarVentana();
+    }
+    private void irARegistro()
+    {
+        frame.dispose();
+        VentanaRegistro ventanaRegistro = new VentanaRegistro();
+        ventanaRegistro.mostrarVentana();
     }
 }
